2e35ca77a6237e9e975340b508d1d231db90f890
esterfogaca <ester@cantarim.com>
2019-03-21 04:18:39 -0300

    Exercício completado!

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import re
 
 def valid_email(email):
-    return False
+    if re.fullmatch(r"(^([A-z0-9\.\-_]+@[A-z0-9]+)\.([A-z]{1,3}(\.[A-z]{1,2})?))", email):
+        return email
 
 
 def filter_email(emails):
-    return []
+#
+#   O return abaixo faz o mesmo que essas linhas comentadas
+#
+#    valid_emails = []
+#    for email in emails:
+#        x = valid_email(email)
+#        if x:
+#            valid_emails.append(x)
+#    return valid_emails
 
+    return [ valid_email(email) for email in emails if valid_email(email) ]
